0df0b275d281bbe45772b460035e09a36a02376f
kathyn15 <113377051+kathyn15@users.noreply.github.com>
2025-07-27 13:19:59 -0500

    Update shell.c

--- shell.c
+++ shell.c
@@ -13,3 +13,21 @@ void printPrompt()
   printf("osshell$ ");
   fflush(stdout);
 }
+
+int main(void) {
+
+
+
+  
+  pid_t processID = fork();
+  if (processID == 0) {
+    // execv call
+
+    exit(1);
+  }
+  if (processID > 0) {
+    wait(NULL);
+  }
+
+}
+
