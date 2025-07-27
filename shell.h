dc13942c4b633a13fa021d42c999a13fe5583550
kathyn15 <113377051+kathyn15@users.noreply.github.com>
2025-07-27 15:57:26 -0500

    Update shell.h

--- shell.h
+++ shell.h
@@ -1,8 +1,20 @@
-//head file to define some variables constants, a struct for command
+/*
+ * File: shell.h
+ * Authors: Group 7: Francisco Ortega, Peter Nguyen, Kathy Nguyen
+ * Course: Operating Systems
+ * Assignment: Simple Shell
+ * Due Date: July 28, 2025
+ *
+ * Description:
+ * We used C programming languagae for the header file for the shell program. 
+   It defines some variables constants, a struct for command, and functions used by the shell.
+ */
+
 
 #ifndef SHELL_H
 #define SHELL_H
 
+// constants to define the max values
 #define LINE 80 //max line length
 #define MAX_ARGS 64 //max num of args
 #define MAX_ARG_LEN 16 //max arg length
@@ -26,8 +38,14 @@ typedef struct {
 
 //prototypes for commands
 void readCommand(char* inputBuffer);
+
+// parses user input into arguments
 void parseCommand(char* inputBuffer, command_t* cmd);
+
+// parses the path into an array
 int parsePath(char* dir[]);
+
+// finds the full path of a command
 char* lookupPath(char* cmdName, char* dir[]);
 
 #endif
