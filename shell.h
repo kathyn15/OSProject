ccf64bb0e8fba34da915afb38fc392e3645fc9b3
kathyn15 <113377051+kathyn15@users.noreply.github.com>
2025-07-27 22:40:01 -0500

    Update shell.h

--- shell.h
+++ shell.h
@@ -1,8 +1,23 @@
+/*
+ * File: shell.h
+ * Authors: Francisco Ortega, Peter Nguyen, Kathy Nguyen
+ * Assignment: Mini Shell
+ * Due date: 07/28/2025
+ *
+ * Description:
+ *   In this program, we have created a header file for the mini shell program.
+ *   It will hold the constants and function prototypes for the shell implementation. 
+ */
+
+
+
 //head file to define some variables constants, a struct for command
 
 #ifndef SHELL_H
 #define SHELL_H
 
+
+// defined max values
 #define LINE 80 //max line length
 #define MAX_ARGS 64 //max num of args
 #define MAX_ARG_LEN 16 //max arg length
@@ -26,8 +41,14 @@ typedef struct {
 
 //prototypes for commands
 void readCommand(char* inputBuffer);
+
+// parse input buffer into command and args
 void parseCommand(char* inputBuffer, command_t* cmd);
+
+// parses path var into array
 int parsePath(char* dir[]);
+
+// search for command in directories and return full path if found
 char* lookupPath(char* cmdName, char* dir[]);
 
 #endif
